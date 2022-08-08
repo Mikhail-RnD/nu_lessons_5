072d7d52ada82a45016ac79de936ce8e474feabe
Mikhail <mtevt@bk.ru>
2022-08-08 21:43:09 +0300

    aa

--- bank_account.py
+++ bank_account.py
@@ -1,14 +1,11 @@
 account_balance = 0
 purchase_history = []
-
-def purchase(balance):
-    purchase = int(input('Введите сумму покупки: '))
-    if purchase < balance:
-        purchase_history.append((input('Введите название покупки: '), purchase))
-        balance -= purchase
-    return balance
-
+# тут без глобальных переменных похоже никак, иначе при втоорм заходе не покажет баланс и список покупок
 def my_banking():
+    global account_balance
+    global purchase_history
+
+
     print('1. пополнение счета')
     print('2. покупка')
     print('3. история покупок')
@@ -16,15 +13,23 @@ def my_banking():
     print('5. выход')
 
     choice = input('Выберите пункт меню: ')
-    if choice == '1':
-        account_balance += int(input('Введите сумму для пополнения: '))
-    elif choice == '2':
-        account_balance = purchase(account_balance)
-    elif choice == '3':
-        print(purchase_history)
-    elif choice == '4':
-        print(f'Баланс счета = {account_balance}')
-    elif choice == '5':
-        return
-    else:
-        print('Неверный пункт меню')
+
+    while choice != '5':
+
+        if choice == '1':
+            account_balance += int(input('Введите сумму для пополнения: '))
+        elif choice == '2':
+            purchase = int(input('Введите сумму покупки: '))
+            if purchase <= account_balance:
+                purchase_history.append((input('Введите название покупки: '), purchase))
+                balance -= purchase
+        elif choice == '3':
+            print(purchase_history)
+        elif choice == '4':
+            print(f'Баланс счета = {account_balance}')
+        elif choice == '5':
+            return
+        else:
+            print('Неверный пункт меню')
+
+        choice = input('Выберите пункт меню: ')
